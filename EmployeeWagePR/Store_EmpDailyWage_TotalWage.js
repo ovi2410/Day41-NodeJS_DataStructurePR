10f035da1fd8002fb0bd970a9a59eb7716eba568
ovi2410 <guneshwarifegade@gmail.com>
2022-08-09 09:12:43 +0530

    Add:UC6_UC8_EmpWageProblems

---
+++ EmployeeWagePR/Store_EmpDailyWage_TotalWage.js
@@ -0,0 +1,44 @@
+// UC6 => Findout EmpDaily wage and Total Wage and Store it in an Array
+
+const MAX_HRS_IN_MONTH = 160;
+const NUM_OF_WORKING_DAYS = 20;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const WAGE_PER_HOUR = 20;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+let empDailyWageArr = new Array();
+
+function calcDailyWage(empHrs) {
+    return empHrs * WAGE_PER_HOUR;
+}
+
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return 4;
+        case IS_FULL_TIME:
+            return 8;
+        default:
+            return 0;
+    }
+}
+
+
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+    totalWorkingDays++;
+
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    if(totalEmpHrs>160)
+    {
+        totalEmpHrs-=empHrs;
+    }
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}
+
+let empWage = calcDailyWage(totalEmpHrs);
+console.log("\n>> Emp Daily Wage stored :- [ "+empDailyWageArr+" ]\n");
+console.log(">> UC6 - Total Days: " + totalWorkingDays + "| Total Hrs: " + totalEmpHrs + "| Emp Wage Total Wage: " + empWage);
